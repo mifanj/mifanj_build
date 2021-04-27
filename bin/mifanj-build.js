0965e79811ed73f641ff277929295f80c773c231
mifanJ <jy2211@qq.com>
2021-04-27 17:56:48 +0800

    add explain word

--- bin/mifanj-build.js
+++ bin/mifanj-build.js
@@ -3,6 +3,6 @@
 process.argv.push("--cwd")
 process.argv.push(process.cwd())
 process.argv.push("--gulpfile")
-process.argv.push(require.resolve(".."))
+process.argv.push(require.resolve("..")) //获取载入文件路径
 
 require("gulp/bin/gulp")
